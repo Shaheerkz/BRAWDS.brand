fbaae26ec23483822a424331b24e3980a1715b77
ShaheerKz <shariikhan925@gmail.com>
2023-08-08 00:37:01 +0500

    feat: currently working on this project for developed more pages  at this site

---
+++ Assests/index.js
@@ -0,0 +1,27 @@
+
+
+
+const slider = document.getElementById("slider");
+const slides = slider.children;
+const imgIndex = slider.children;
+imgIndex[0].className = "Active";
+function prev() {
+    for (let i = 0; i < imgIndex.length; i++) {
+        if (imgIndex[i].className === "Active") {
+            imgIndex[i].className = "";
+            imgIndex[i - 1].className = "Active";
+            slider.style.transform = "translateX(" + (i - 1) * -187 + "px)";
+            break;
+        }
+    }
+};
+function next() {
+    for (let x = 0; x < imgIndex.length; x++) {
+        if (imgIndex[x].className === "Active") {
+            imgIndex[x].className = "";
+            imgIndex[x + 1].className = "Active";
+            slider.style.transform = "translateX(" + (x + 1) * -187 + "px)";
+            break;
+        }
+    }
+};
